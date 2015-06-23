70c4c3a790e80e2f47b4261ec523d493dac43286
Gordon DuGan <gordon.dugan2@gmail.com>
2015-06-23 14:19:16 -0400

    removed bad migration

--- profiles/db/schema.rb
+++ profiles/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623171638) do
+ActiveRecord::Schema.define(version: 20150623180558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 20150623171638) do
     t.integer "skill_id", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: false, force: :cascade do |t|
     t.string   "f_name"
     t.string   "l_name"
     t.string   "org_name"
