d6ebc84d7b8c4a8839f39aec141c1a250e9a5296
Jeremy Bell <jeremyianbell@gmail.com>
2015-06-23 13:13:43 -0400

    seeded users for project 3

--- profiles/db/seeds.rb
+++ profiles/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+
+user_jeremy = User.new([{f_name: 'Jeremy'}, {l_name: 'Bell'}, {email: 'jeremyianbell@gmail.com'}, {password_digest: 'melville1'}, {user_type: 'student'}
+	])
+user_jeremy.save
+
+user_karlyce = User.new([{f_name: 'Karlyce'}, {l_name: 'Edwards'}, {email: 'karlyce.edwards@gmail.com'}, {password_digest: 'melville1'}, {user_type: 'student'}
+	])
+user_karlyce.save
+
+
+user_gordon = User.new([{f_name: 'Gordon'}, {l_name: 'DuGan'}, {email: 'gordon.dugan2@gmail.com'}, {password_digest: 'melville1'}, {user_type: 'student'}
+	])
+user_gordon.save
+
+
+user_fernanda = User.new([{f_name: 'Fernanda'}, {l_name: 'Correa'}, {email: 'nandasc@gmail.com'}, {password_digest: 'melville1'}, {user_type: 'student'}
+	])
+user_fernanda.save
+
+user_drew = User.new([{f_name: 'Drew'}, {l_name: 'Garver'}, {email: 'a.w.garver@gmail.com'}, {password_digest: 'melville1'}, {user_type: 'student'}
+	])
+user_drew.save
+
+
