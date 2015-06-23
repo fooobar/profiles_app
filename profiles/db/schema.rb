bf8e2cd9a7e468bc9dca4878954a0b9f5c2ca717
Gordon DuGan <gordon.dugan2@gmail.com>
2015-06-23 14:34:59 -0400

    I don't even know anymore

--- profiles/db/schema.rb
+++ profiles/db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150623175004) do
+ActiveRecord::Schema.define(version: 20150623180558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +58,9 @@ ActiveRecord::Schema.define(version: 20150623175004) do
   end
 
   create_table "skills", force: :cascade do |t|
-    t.string "name"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "skills_users", force: :cascade do |t|
--- profiles/db/seeds.rb
+++ profiles/db/seeds.rb
@@ -8,25 +8,25 @@
 
 User.destroy_all
 
-user_jeremy = User.new([{f_name: 'Jeremy'}, {l_name: 'Bell'}, {email: 'jeremyianbell@gmail.com'}, {password: 'melville1'}, {type: 'student'}
+user_jeremy = User.create([{f_name: 'Jeremy'}, {l_name: 'Bell'}, {email: 'jeremyianbell@gmail.com'}, {password: 'melville1'}, {type: 'student'}
 	])
 user_jeremy.save
 
-user_karlyce = User.new([{f_name: 'Karlyce'}, {l_name: 'Edwards'}, {email: 'karlyce.edwards@gmail.com'}, {password: 'melville1'}, {type: 'student'}
+user_karlyce = User.create([{f_name: 'Karlyce'}, {l_name: 'Edwards'}, {email: 'karlyce.edwards@gmail.com'}, {password: 'melville1'}, {type: 'student'}
 	])
 user_karlyce.save
 
 
-user_gordon = User.new([{f_name: 'Gordon'}, {l_name: 'DuGan'}, {email: 'gordon.dugan2@gmail.com'}, {password: 'melville1'}, {type: 'student'}
+user_gordon = User.create([{f_name: 'Gordon'}, {l_name: 'DuGan'}, {email: 'gordon.dugan2@gmail.com'}, {password: 'melville1'}, {type: 'student'}
 	])
 user_gordon.save
 
 
-user_fernanda = User.new([{f_name: 'Fernanda'}, {l_name: 'Correa'}, {email: 'nandasc@gmail.com'}, {password: 'melville1'}, {type: 'student'}
+user_fernanda = User.create([{f_name: 'Fernanda'}, {l_name: 'Correa'}, {email: 'nandasc@gmail.com'}, {password: 'melville1'}, {type: 'student'}
 	])
 user_fernanda.save
 
-user_drew = User.new([{f_name: 'Drew'}, {l_name: 'Garver'}, {email: 'a.w.garver@gmail.com'}, {password: 'melville1'}, {type: 'student'}
+user_drew = User.create([{f_name: 'Drew'}, {l_name: 'Garver'}, {email: 'a.w.garver@gmail.com'}, {password: 'melville1'}, {type: 'student'}
 	])
 user_drew.save
 
