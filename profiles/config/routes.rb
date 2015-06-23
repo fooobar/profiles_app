a1a4e4b91e59d90877f0b0994ddcbd2b06224f48
Fernanda Correa <nandasc@gmail.com>
2015-06-23 12:40:06 -0400

    craeting authentication

--- profiles/config/routes.rb
+++ profiles/config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
 
   root "welcome#index"
 
+  get     '/login' =>   'sessions#new'
+  post    '/login' =>   'sessions#create'
+  delete  '/logout' =>  'sessions#destroy'
 
+  resources :sessions, only: [ :new, :create, :destroy]
+
+  resources :users
+
+  
 end
