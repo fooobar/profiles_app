6fa64f97cb6c3e7ca514666389d53c5212ed32d1
Fernanda Correa <nandasc@gmail.com>
2015-06-23 14:50:05 -0400

    changes to seed

--- profiles/db/seeds.rb
+++ profiles/db/seeds.rb
@@ -3,26 +3,50 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   cities = City.create({ name: 'Chicago' , name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.destroy_all
 
-user_jeremy = User.create([{f_name: 'Jeremy'}, {l_name: 'Bell'}, {email: 'jeremyianbell@gmail.com'}, {password: 'melville1'}, {type: 'student'}
-	])
+user_jeremy = User.create({
+	f_name: 'Jeremy',
+	l_name: 'Bell',
+	email: 'jeremyianbell@gmail.com', 
+	password: 'melville1', user_type: 'student'
+	})
 
-user_karlyce = User.create([{f_name: 'Karlyce'}, {l_name: 'Edwards'}, {email: 'karlyce.edwards@gmail.com'}, {password: 'melville1'}, {type: 'student'}
-	])
+user_karlyce = User.create({
+	f_name: 'Karlyce', 
+	l_name: 'Edwards', 
+	email: 'karlyce.edwards@gmail.com',
+	password: 'melville1', 
+	user_type: 'student'
+	})
 
 
-user_gordon = User.create([{f_name: 'Gordon'}, {l_name: 'DuGan'}, {email: 'gordon.dugan2@gmail.com'}, {password: 'melville1'}, {type: 'student'}
-	])
+user_gordon = User.create({
+	f_name: 'Gordon', 
+	l_name: 'DuGan', 
+	email: 'gordon.dugan2@gmail.com', 
+	password: 'melville1', 
+	user_type: 'student'
+	})
 
 
-user_fernanda = User.create([{f_name: 'Fernanda'}, {l_name: 'Correa'}, {email: 'nandasc@gmail.com'}, {password: 'melville1'}, {type: 'student'}
-	])
+user_fernanda = User.create({
+	f_name: 'Fernanda',
+	l_name: 'Correa',
+	email: 'nandasc@gmail.com',
+	password: 'melville1',
+	user_type: 'student'
+})
 
-user_drew = User.create([{f_name: 'Drew'}, {l_name: 'Garver'}, {email: 'a.w.garver@gmail.com'}, {password: 'melville1'}, {type: 'student'}
-	])
+user_drew = User.create({
+	f_name: 'Drew',
+	l_name: 'Garver',
+	email: 'a.w.garver@gmail.com',
+	password: 'melville1',
+	user_type: 'student'
+	})
 
 
