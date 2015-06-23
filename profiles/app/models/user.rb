036a0cccbdacf152b595bd68c2265caae87808bf
Fernanda Correa <nandasc@gmail.com>
2015-06-23 15:20:26 -0400

    models update

--- profiles/app/models/user.rb
+++ profiles/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-	# has_many :skills, :jobs, :projects, :work_experience
+	has_many :jobs, :projects, :work_experience
+	has_and_belongs_to_many :skills
 
 	has_secure_password
 end
