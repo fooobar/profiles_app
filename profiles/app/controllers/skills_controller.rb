b90e9b80e4340e980de38d68d75741dbd79fb36e
Gordon DuGan <gordon.dugan2@gmail.com>
2015-06-25 13:40:32 -0400

    updated controllers

--- profiles/app/controllers/skills_controller.rb
+++ profiles/app/controllers/skills_controller.rb
@@ -1,17 +1,30 @@
 class SkillsController < ApplicationController
 
-	def new
-		@skill = Skill.new(skill_params)
-		if @skill.save
-			render json: @skill
-		else
-			flash[:alert] = "Unable to save skill"
+	def create
+		skill = Skill.new(skill_params)
+		skills = Skill.all
+		user = User.find(params[:user_id])
+		exists = false
+		user_has_skill = false
+		skills.each do |existing_skill|
+			if exiting_skill.name == skill.name
+				exists = true
+			end
 		end
-	end
-
-	def update
-		@user = User.find(params[:user_id])
-		@skill = Skill.find(params[:id])
+		if exists
+			render json: skill
+		else
+			skill.save
+			User.skills.each do |user_skill|
+				if user_skill.name == skill.name
+					user_has_skill = true
+				end
+			end
+			unless user_has_skill
+				user.skills.push(skill)
+			end
+			render json: skill
+		end
 	end
 
 	private
