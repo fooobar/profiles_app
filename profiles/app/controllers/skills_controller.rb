9481e6059eb8faef670c2db06ee931ee0c3d1718
Gordon DuGan <gordon.dugan2@gmail.com>
2015-06-26 13:32:59 -0400

    updated params in controllers

--- profiles/app/controllers/skills_controller.rb
+++ profiles/app/controllers/skills_controller.rb
@@ -37,7 +37,7 @@ class SkillsController < ApplicationController
 
 	private
 		def skill_params
-			params.require(:skill).permit(:name)
+			params.require(:skill).permit(:name, :clicked)
 		end
 	end
 end
