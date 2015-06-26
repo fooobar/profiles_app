b64bb07221bb0b62aebcca67731ca32a46da399b
Karlyce Edwards <karlyce.edwards@gmail.com>
2015-06-26 11:19:13 -0400

    fixed job board filter

--- profiles/app/assets/javascripts/jobBoard.js
+++ profiles/app/assets/javascripts/jobBoard.js
@@ -1,39 +1,34 @@
 if ($('body.jobs.index').length) {
-
-	// select all the checkboxes on the page and check their status
-	//Hide the unchecked and show the checked
-	//if none are checked show all jobs
-	var showChecked = function(){
-		$(".jobs").hide();
-		checkbox = $('input[type=checkbox]')
-		if ($( "input:checked" ).length !== 0){
-					$(".jobs").hide();
-					var checkboxes = $("input[type=checkbox]:checked")
-					var classes = checkboxes.map(function(i, checkbox) {
-						return checkbox.name
-					}).get();
-					$('.'+classes.join(',.')).show();
-		} else {
-			for (var i = 0; i < checkbox.length; i++){
-				$('.'+checkbox[i].name).show()
-			}
-		}
-	}	
-
-	//rendering the data list to the page
-	var renderView = function(dataList){
-		$('#listing-container').text('')
-		var template = $('#template').html()
-		var render = Mustache.render(template, dataList)
-		$('#listing-container').append(render)
+// select all the checkboxes on the page and check their status
+//Hide the unchecked and show the checked
+//if none are checked show all jobs
+var showChecked = function(){
+	checkbox = $('input[type=checkbox]')
+	if ($( "input:checked" ).length !== 0){
+			$(".positions").hide();
+			var checkboxes = $("input[type=checkbox]:checked")
+			var classes = checkboxes.map(function(i, checkbox) {
+			return checkbox.name
+			}).get();
+				$('.'+classes.join(',.')).show();
+	} else {
+			$(".positions").show();
 	}
-
-
-	renderView(data)
-
-	//When a checkbox is changed, the page updates
-	$("input[type=checkbox]").on('change',function(event){
-		showChecked()
-	});
 }
 
+//rendering the job list to the page
+var renderUsers = function(dataList){
+	$('#listing-container').text('')
+	var template = $('#jobs-template').html()
+	var render = Mustache.render(template, dataList)
+	$('#listing-container').append(render)
+}
+
+renderUsers(data)
+
+//When a checkbox is changed, the page updates
+$("input[type=checkbox]").on('change',function(event){
+	showSkill = event.target.name
+	showChecked()
+});
+}
