25d4d1d2002b318303ea1247dbe204ae321a1690
Karlyce Edwards <karlyce.edwards@gmail.com>
2015-06-26 10:47:23 -0400

    updated job board filters

--- profiles/app/assets/javascripts/jobBoard.js
+++ profiles/app/assets/javascripts/jobBoard.js
@@ -1,16 +1,17 @@
+	if ($('body.jobs.index').length) {
 	// select all the checkboxes on the page and check their status
 	//Hide the unchecked and show the checked
 	//if none are checked show all jobs
 	var showChecked = function(){
+		$(".jobs").hide();
 		checkbox = $('input[type=checkbox]')
 		if ($( "input:checked" ).length !== 0){
-			for (var i = 0; i < checkbox.length; i++){
-				if (checkbox[i].checked == false){
-					$('.'+checkbox[i].name).hide()
-				} else {
-					$('.'+checkbox[i].name).show()
-				}
-			} 
+					$(".jobs").hide();
+					var checkboxes = $("input[type=checkbox]:checked")
+					var classes = checkboxes.map(function(i, checkbox) {
+						return checkbox.name
+					}).get();
+					$('.'+classes.join(',.')).show();
 		} else {
 			for (var i = 0; i < checkbox.length; i++){
 				$('.'+checkbox[i].name).show()
@@ -32,3 +33,4 @@
 	$("input[type=checkbox]").on('change',function(event){
 		showChecked()
 	});
+}
